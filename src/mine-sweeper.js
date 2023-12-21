d2b6ee13e4fcd5e70c25ca2d7aef38da48740cb7
andnikis <andreymon@gmail.com>
2023-12-21 23:25:37 +0600

    feat: do 15 tasks

--- src/mine-sweeper.js
+++ src/mine-sweeper.js
@@ -23,9 +23,46 @@ const { NotImplementedError } = require('../extensions/index.js');
  *  [1, 1, 1]
  * ]
  */
-function minesweeper(/* matrix */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function minesweeper(matrix) {
+  // all rows have the same size
+  const MIN_ROW = 0;
+  const MAX_ROW = matrix.length - 1;
+  // all columns have the same size
+  const MIN_COLUMN = 0;
+  const MAX_COLUMN = matrix[0].length - 1;
+  // result
+  let map = new Array(MAX_ROW + 1);
+  for (let i = 0; i <= MAX_ROW; i++) map[i] = new Array(MAX_COLUMN + 1);
+
+  for (let i = 0; i < matrix.length; i++) {
+    for (let j = 0; j < matrix[i].length; j++) {
+      let minesCount = 0;
+      // check the upper row
+      minesCount += calcRowMines(i - 1, j);
+      // check the current row
+      minesCount += calcRowMines(i, j);
+      // check the lower row
+      minesCount += calcRowMines(i + 1, j);
+      // don't calc a mine in the same cell
+      map[i][j] = matrix[i][j] ? minesCount - 1 : minesCount;
+    }
+  }
+
+  function calcRowMines(row, column) {
+    let minesInRow = 0;
+
+    // indexes must be within the range
+    if (row >= MIN_ROW && row <= MAX_ROW) {
+      for (let j = column - 1; j <= column + 1; j++) {
+        if (j >= MIN_COLUMN && j <= MAX_COLUMN && matrix[row][j])
+          minesInRow++;
+      }
+    }
+
+    return minesInRow;
+  }
+
+  return map;
 }
 
 module.exports = {
--- src/simple-chain.js
+++ src/simple-chain.js
@@ -1,32 +1,51 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require("../extensions/index.js");
 
 /**
  * Implement chainMaker object according to task description
- * 
+ *
  */
 const chainMaker = {
+  chain: [],
+
   getLength() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return this.chain.length;
   },
-  addLink(/* value */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+
+  addLink(value) {
+    // undefined isn't checked in tests
+    this.chain.push(value === undefined ? "" : value);
+
+    return this;
   },
-  removeLink(/* position */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+
+  removeLink(position) {
+    // position is from 1 to getLength(), not zero based
+    if (typeof position !== "number" || !Number.isInteger(position) || position < 1 || position > this.getLength()) {
+      this.clearChain();
+      throw Error("You can't remove incorrect link!");
+    }
+
+    this.chain.splice(position - 1, 1);
+    return this;
   },
+
   reverseChain() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    this.chain.reverse();
+    return this;
   },
+
+  clearChain() {
+    this.chain.length = 0;
+  },
+
   finishChain() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
+    let text = this.chain.map((v) => `( ${v} )`).join('~~');
+
+    this.clearChain();
+    return text;
+  },
 };
 
 module.exports = {
-  chainMaker
+  chainMaker,
 };
